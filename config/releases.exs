0e3dca57c4badd4bef7a2334ac299ab4271b55ba
Henry Warren <henrywarren@Henrys-MacBook-Pro.local>
2021-03-02 09:39:57 -0500

    Prepare for Mix Releases for Render Hosting
    - Create necessary configuration for mix release deploys on Render.com

--- config/releases.exs
+++ config/releases.exs
@@ -2,7 +2,7 @@
 # from environment variables. You can also hardcode secrets,
 # although such is generally not recommended and you have to
 # remember to add this file to your .gitignore.
-use Mix.Config
+import Config
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
@@ -23,7 +23,7 @@ config :progressions, MidimatchesWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :midimatches, MidimatchesWeb.Endpoint, server: true
+config :progressions, MidimatchesWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
