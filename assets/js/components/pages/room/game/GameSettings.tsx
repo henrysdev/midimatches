a8dc32817bc4a3ce210fa2886583158907ca109c
Henry Warren <henrywarren@Henrys-MacBook-Pro.local>
2021-03-08 19:44:10 -0500

    Added Volume Slider
    - Implemented volume slider and added to settings pane

--- assets/js/components/pages/room/game/GameSettings.tsx
+++ assets/js/components/pages/room/game/GameSettings.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 
 import { useToneAudioContext } from "../../../../hooks";
 import { MidiConfiguration } from "../../../audio";
@@ -14,13 +14,21 @@ const GameSettings: React.FC<GameSettingsProps> = ({}) => {
     setDisabledMidiInputIds,
     originalMidiInputs,
   } = useToneAudioContext();
-  const [soundIsOn, setSoundIsOn] = useState<boolean>(Tone.Master.mute);
-
-  const toggleMute = () => {
-    const newIsSoundOn = !soundIsOn;
-    Tone.Master.mute = newIsSoundOn;
-    setSoundIsOn(newIsSoundOn);
+  const [currVolume, setCurrVolume] = useState<string>("-1");
+  const handleVolumeChange = (e: any) => {
+    const volume = e.target.value;
+    setCurrVolume(volume);
   };
+
+  useEffect(() => {
+    const volume = parseFloat(currVolume);
+    Tone.Master.volume.value = volume / 10.0;
+    Tone.Master.mute = volume === -100;
+  }, [currVolume]);
+
+  const soundIsOn = useMemo(() => {
+    return currVolume === "-100";
+  }, [currVolume]);
 
   return (
     <div className="in_game_settings_pane inline_screen">
@@ -31,26 +39,31 @@ const GameSettings: React.FC<GameSettingsProps> = ({}) => {
         disabledMidiInputIds={disabledMidiInputIds}
         setDisabledMidiInputIds={setDisabledMidiInputIds}
       />
-      <h5 className="settings_item_label">Sound</h5>
+      <h5 className="settings_item_label">Volume</h5>
       <ul style={{ listStyleType: "none", paddingLeft: 0 }}>
         <li style={{ padding: 0 }}>
-          {soundIsOn ? (
-            <i style={{ verticalAlign: "middle" }} className="material-icons">
-              volume_off
-            </i>
-          ) : (
-            <i style={{ verticalAlign: "middle" }} className="material-icons">
-              volume_up
-            </i>
-          )}
-          <label className="switch">
+          <div className="volume_slider_container">
+            {soundIsOn ? (
+              <i
+                style={{ verticalAlign: "middle", color: "red" }}
+                className="material-icons"
+              >
+                volume_off
+              </i>
+            ) : (
+              <i style={{ verticalAlign: "middle" }} className="material-icons">
+                volume_up
+              </i>
+            )}
             <input
-              type="checkbox"
-              checked={!soundIsOn}
-              onChange={() => toggleMute()}
+              type="range"
+              min="-100"
+              max="10"
+              value={currVolume}
+              onChange={handleVolumeChange}
+              className="volume_slider"
             />
-            <span className="slider round"></span>
-          </label>
+          </div>
         </li>
       </ul>
     </div>
