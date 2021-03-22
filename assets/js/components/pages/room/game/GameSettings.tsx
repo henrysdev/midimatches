3882ab0324749f465a3f9c32b245dba66f8b384c
Henry Warren <henrywarren@Henrys-MacBook-Pro.local>
2021-03-22 17:26:00 -0400

    Renamed Keyboard Labels in Game Settings

--- assets/js/components/pages/room/game/GameSettings.tsx
+++ assets/js/components/pages/room/game/GameSettings.tsx
@@ -92,7 +92,7 @@ const GameSettings: React.FC<GameSettingsProps> = ({}) => {
           </div>
         </li>
       </ul>
-      <h5 className="settings_item_label">Keyboard Labels</h5>
+      <h5 className="settings_item_label">Computer Keyboard Hints</h5>
       <label className="switch">
         <input
           type="checkbox"
