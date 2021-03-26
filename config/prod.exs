94451291c86d9d608df02f0c7820ee2be08733c6
Henry Warren <henrywarren@Henrys-MacBook-Pro.local>
2021-03-26 10:42:59 -0400

    Set host origin to default
    - Set host to nil so that both midimatches.com and
    midimatches.onrender.com resolve properly for all protocols

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :midimatches, MidimatchesWeb.Endpoint,
   url: [
-    host: "midimatches.com",
+    host: nil,
     port: 80
   ],
   cache_static_manifest: "priv/static/cache_manifest.json"
