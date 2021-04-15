c1c180eebe8cecfe39a2ed9a860c5a433cdbea5c
Henry Warren <henrywarren@Henrys-MBP.lan>
2021-04-15 11:53:39 -0500

    Start Impl of Storing Backing Tracks in DB

---
+++ test/midimatches_db/backing_tracks_test.exs
@@ -0,0 +1,65 @@
+defmodule MidimatchesDb.BackingTracksTest do
+  use MidimatchesDb.RepoCase
+
+  alias MidimatchesDb.{
+    BackingTrack,
+    BackingTracks
+  }
+
+  test "create backing track" do
+    backing_track = %BackingTrack{
+      name: "jazzy",
+      file_url: "https://jazzzzziness/jazzy.com",
+      bpm: 90,
+      musical_key: "Em",
+      author: "mr funkadelic"
+    }
+
+    inserted_track = BackingTracks.create_backing_track(backing_track)
+
+    expected_track = %BackingTrack{
+      name: "jazzy",
+      file_url: "https://jazzzzziness/jazzy.com",
+      bpm: 90,
+      musical_key: "Em",
+      author: "mr funkadelic"
+    }
+
+    assertion_fields = [:author, :bpm, :file_url, :musical_key, :name]
+
+    Enum.each(assertion_fields, fn field ->
+      assert Map.get(inserted_track, field) == Map.get(expected_track, field)
+    end)
+  end
+
+  describe "fetch random backing tracks" do
+    test "for specified count" do
+      populate_backing_tracks_table(10)
+
+      fetched_tracks = BackingTracks.fetch_random_backing_tracks(4)
+
+      assert length(fetched_tracks) == 4
+    end
+
+    test "when count is greater than number of total tracks should return all tracks" do
+      populate_backing_tracks_table(10)
+
+      fetched_tracks = BackingTracks.fetch_random_backing_tracks(40)
+
+      assert length(fetched_tracks) == 10
+    end
+  end
+
+  defp populate_backing_tracks_table(count) do
+    for _ <- 1..count do
+      %BackingTrack{
+        name: UUID.uuid4(),
+        file_url: UUID.uuid4(),
+        author: UUID.uuid4(),
+        bpm: 90,
+        musical_key: "Em"
+      }
+      |> BackingTracks.create_backing_track()
+    end
+  end
+end
